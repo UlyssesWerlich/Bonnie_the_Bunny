3ae48656ab9bf46fc2e97e2d33b3ec27f098c11e
UlyssesWerlich <ulysses.borges@aluno.usj.edu.br>
2020-10-22 20:05:46 -0300

    Versão 0.1

---
+++ Scripts/Bee.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bee : MonoBehaviour
+{
+    
+    public Rigidbody2D beeRB;
+    public float velocity;
+    public float flipTime;
+
+    private float flipCounter;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        flipCounter += Time.deltaTime;
+
+        if(flipCounter >= flipTime){
+            velocity = -velocity;
+            beeRB.velocity = new Vector2 (velocity, beeRB.velocity.y);
+            flipCounter = 0f;
+        }
+
+        if (velocity > 0){
+            transform.eulerAngles = new Vector2(0f, 180f);
+        } else if (velocity < 0){
+            transform.eulerAngles = new Vector2(0f, 0f);
+        }
+    }
+}
---
+++ Scripts/Player.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public float velocity;
+    public Rigidbody2D playerRB;
+    public float jumpForce;
+    public Animator animator;
+    public Transform cam;
+
+    private bool isJumping; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        cam.position = new Vector3 (transform.position.x, transform.position.y, cam.position.z);
+        Move();
+        Jump();
+    }
+
+    void Move(){
+        float direction = Input.GetAxis("Horizontal");
+        playerRB.velocity = new Vector2( direction * velocity, playerRB.velocity.y );
+
+        Flip(direction);
+    }
+
+    void Flip(float direction){
+        if (direction > 0f ){
+            transform.eulerAngles = new Vector2(0f, 0f);
+
+            if(!isJumping)
+                animator.SetInteger("Transition", 1);
+        }
+
+        if (direction < 0f ){
+            transform.eulerAngles = new Vector2(0f, 180f);
+
+            if(!isJumping)
+                animator.SetInteger("Transition", 1);        
+        }
+
+        if (direction == 0f){
+            if(!isJumping)
+                animator.SetInteger("Transition", 0);
+        }
+    }
+    void Jump(){
+        if (Input.GetButtonDown("Jump") && !isJumping){
+            isJumping = true;
+            playerRB.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+            animator.SetInteger("Transition", 2);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision){
+        if(collision.gameObject.layer == 8){
+            isJumping = false;
+            animator.SetInteger("Transition", 0);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision){
+        if (collision.CompareTag("Bee")){
+
+        }
+
+        if (collision.CompareTag("BeeHead")){
+            Destroy(collision.transform.parent.gameObject);
+            playerRB.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+        }
+    }
+}
